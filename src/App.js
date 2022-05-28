f9d731fe86cc447b42c91a15d516ceb2b689c520
Daria Cook <nekonohimitsu@hotmail.com>
2022-05-28 15:52:04 -0400

    example

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ContactCard />
+      <ContactCard />
+      <ContactCard />
+    </>
   );
 }
 
 export default App;
+
+const ContactCard = () => {
+  return (
+    <div className="contact-card">
+      <img src="https://via.placeholder.com/150" alt="profile" />
+      <div className="user-details">
+        <p>Name: Jenny Han</p>
+        <p>Email: Jenny.Han@notreal.com</p>
+        <p>Age: 25</p>
+      </div>
+    </div>
+  );
+};
