d3fe963bf85e530a305d59b7523f5795975e0536
Daria Cook <nekonohimitsu@hotmail.com>
2022-05-29 16:39:14 -0400

    starting point

--- packages/components/ui-infrastructure/src/components/app/standard-app.tsx
+++ packages/components/ui-infrastructure/src/components/app/standard-app.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import FFXIQueryToolStore from '@ffxi-query-tool/store';
+import { FFXIQueryToolStore } from '@ffxi-query-tool/store';
 
 class StandardApp extends React.Component {
     public render(): React.ReactNode {
         return (
-            <Provider store={FFXIQueryToolStore.getStore()}>
+            <Provider store={FFXIQueryToolStore}>
             </Provider>
         );
     }
