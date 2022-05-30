d535adc8300ca039284ddf0fe4b28b6be4e868fd
Daria Cook <nekonohimitsu@hotmail.com>
2022-05-30 19:25:57 -0400

    add ci command

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ const config = {
     testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
     moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
     collectCoverageFrom: ["packages/**/src/**/*.{js,ts,jsx,tsx}","ffxi-query-tool-app/src/**/*.{js,ts,jsx,tsx}"],
-    coveragePathIgnorePatterns: ["index.ts", "index.tsx", "node_modules/*"],
+    coveragePathIgnorePatterns: ["index.ts", "index.tsx"],
     collectCoverage: true,
     coverageReporters: [
         "text-summary",
