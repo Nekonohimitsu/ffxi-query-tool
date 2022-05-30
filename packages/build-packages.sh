b92828255e6f75a2a52b54a9f8df8be4f4ed4220
Daria Cook <nekonohimitsu@hotmail.com>
2022-05-29 23:42:55 -0400

    another reformatting

--- packages/build-packages.sh
+++
@@ -1,2 +0,0 @@
-yarn install --force --network-timeout=1000000 || exit
-npx oao run-script --tree --parallel --ignore-src **/node_modules/** build
---
+++ workspace/build-packages.sh
@@ -0,0 +1,3 @@
+yarn install --force --network-timeout=1000000 || exit
+npx oao run-script --tree --parallel --ignore-src **/node_modules/** build
+read
