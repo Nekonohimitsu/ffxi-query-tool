fd1333600807220fe5f966aa9c02916d5a59ef0a
Daria Cook <nekonohimitsu@hotmail.com>
2022-05-30 17:41:54 -0400

    updates

--- packages/first-component/jest.config.js
+++ packages/first-component/jest.config.js
@@ -1,11 +1,11 @@
-const config = { 
+const config = {
     transform: {
-        "^.+\\.tsx?$": "ts-jest"
+        '^.+\\.tsx?$': 'ts-jest',
     },
-    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
-    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-    collectCoverageFrom: ["src/**/*.{js,ts,jsx,tsk}"],
-    coveragePathIgnorePatterns: ["<rootDir>/src/index.ts", "<rootDir>/node_modules/"],
-    collectCoverage: true
-}
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    collectCoverageFrom: ['src/**/*.{js,ts,jsx,tsk}'],
+    coveragePathIgnorePatterns: ['<rootDir>/src/index.ts', '<rootDir>/node_modules/'],
+    collectCoverage: true,
+};
 module.exports = config;
